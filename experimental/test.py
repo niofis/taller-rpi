9a4e6b8cd0695483415da61a568da8b715e837a3
Enrique <niofis@gmail.com>
2020-02-19 09:28:34 -0700

    deleted old files

--- experimental/test.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from remote import initConnection, get_geolocation, get_orientation 
-
-initConnection(url='https://127.0.0.1:3443', id=1234)
-while True:
-    print(get_orientation())
-    print(get_geolocation())
-    sleep(0.1)
